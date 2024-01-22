10458983c06ccaab7c89f7cc678de581340208c9
TaillepierreN <taillepierren@gmail.com>
2024-01-22 05:50:12 -0800

    change 2-print_alphabet,to avoid storing characters in a variable

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-alphabet = ""
 for letter in range(97, 123):
 	if letter != 101 and letter != 113:
-		alphabet += chr(letter)
-print(alphabet)
+		print(chr(letter), end="")
+print()
