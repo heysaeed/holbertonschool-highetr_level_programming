3934b4b5a3043884a1c91724541f9ecb5a58b69f
TaillepierreN <taillepierren@gmail.com>
2024-01-22 06:54:08 -0800

    Added 8-uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+	for letter in str:
+		if (ord(letter) <= ord('z') and ord(letter) >= ord('a')):
+			upper_char = chr(ord(letter) - 32)
+		else:
+			upper_char = letter
+		print(f"{upper_char}", end="")
+	print('')
