a3c01ec2e734f7e69fa47bc1bd6f09e95fc4a952
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-23 09:36:00 +0100

    8-uppercase, took print out of loop

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+	upper_char = ''
 	for letter in str:
 		if (ord(letter) <= ord('z') and ord(letter) >= ord('a')):
-			upper_char = chr(ord(letter) - 32)
+			upper_char += chr(ord(letter) - 32)
 		else:
-			upper_char = letter
-		print(f"{upper_char}", end="")
-	print('')
+			upper_char += letter
+	print(upper_char)
