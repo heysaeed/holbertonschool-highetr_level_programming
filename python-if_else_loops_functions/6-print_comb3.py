92aeab9365ced7c894b2a28e57261dda9b1906d9
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-23 11:20:54 +0100

    6-print_comb3 added missing curly braces

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for firstDigit in range(0, 9):
     for secondDigit in range(firstDigit + 1, 10):
         if (firstDigit != 8 or secondDigit != 9):
-            print("{}".format(firstDigit, secondDigit), end=", ")
+            print("{}{}".format(firstDigit, secondDigit), end=", ")
         else:
-            print("{}".format(firstDigit, secondDigit))
+            print("{}{}".format(firstDigit, secondDigit))
