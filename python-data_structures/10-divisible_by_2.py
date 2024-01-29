2918c7b75fd812d0a85eeaf4bde7eee2596d8062
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-29 17:11:16 +0100

    added 10-divisible_by_2 code

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    newlist = []
+    for i in my_list:
+        if i % 2 == 0:
+            newlist.append(True)
+        else:
+            newlist.append(False)
+    return newlist
