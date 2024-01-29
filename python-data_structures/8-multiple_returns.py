7a11f34d9ee65f9323dd7f10199858cc35e5138e
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-29 17:10:50 +0100

    added 8-multiple_returns code

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if not sentence:
+        firstChar = None
+    else:
+        firstChar = sentence[0]
+    return (len(sentence), firstChar)
