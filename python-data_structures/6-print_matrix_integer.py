ac772aff4c0949c66de964b59e0401da6f4eea93
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-29 16:08:58 +0100

    Added 6-print_matrix_integer

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end=' ' if i != len(row) - 1 else '')
+        print()
