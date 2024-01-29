163a917826cbfe6006cbc3b8f32dd71f341acd87
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-29 14:20:27 +0100

    added 3-print_reversed_list_integer code

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    length = len(my_list) - 1
+    while length >= 0:
+        print(my_list[length])
+        length = length - 1
