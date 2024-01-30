7a2dbc1fcadfc0f96cc6efc5b5859c4f1c4dcf82
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-30 16:10:57 +0100

    Added 12-roman_to_int

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if roman_string == "" or roman_string is None:
+        return 0
+    numerals = {'I': 1, 'V': 5, 'X': 10,
+                'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_value = 0
+    prev_value = 0
+    for letter in reversed(roman_string):
+        value = numerals[letter]
+        if value < prev_value:
+            int_value -= value
+        else:
+            int_value += value
+        prev_value = value
+    return int_value
