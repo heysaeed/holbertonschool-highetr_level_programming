81bba9592afaeb2af942ffd674d2d8d2d932eb79
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-30 10:40:20 +0100

    added 0-square_matrix_simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,8 @@
-a
+#!/usr/bin/python3
 
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_matrix_row = list(map(lambda x: x ** 2, row))
+        new_matrix.append(new_matrix_row)
+    return new_matrix
