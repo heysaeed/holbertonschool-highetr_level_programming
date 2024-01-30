6772b53b30b363e272a9bfdb583312ed4bb97ac9
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-30 16:34:03 +0100

    change string check in 12-roman_to_int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if roman_string == "" or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     numerals = {'I': 1, 'V': 5, 'X': 10,
                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
