ca022158d0b3fc31688470bd7b0f2b61cbb77d19
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-31 11:38:38 +0100

    2-safe_print_list_integers remove indexError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             printed += 1
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             continue
     print("")
     return printed
