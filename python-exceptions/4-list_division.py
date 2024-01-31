8e0f24ef7068f77582c375c62854c07a02652d72
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-31 10:52:17 +0100

    4-list_division removed uppercase in string

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
-            print("Wrong type")
+            print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
         except IndexError:
