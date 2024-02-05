33d27e6a5e2bee7d39c649cd166f428533aec655
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-05 16:57:29 +0100

    Added 4-square

---
+++ python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Define a square class
+"""
+
+
+class Square:
+    """
+    define attribute of square
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns:
+            int: return square of size
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
