2498e3ec7f3d980e752a8f7df4f8cdf2fef92869
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-05 14:26:30 +0100

    Added 2-square

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Define a square class
+"""
+
+
+class Square:
+    """
+    define attribute of square
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
