4348f4f34142cc05820f0e58b799c7ad7ac8713d
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-05 11:52:21 +0100

    Added 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Define a square class
+"""
+
+
+class Square:
+    """
+    define attribute of square
+    """
+
+    def __init__(self, size):
+        self.__size = size
