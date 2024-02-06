81c4543e58c358be4b17939b5a8bd5c43d5d2da9
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-06 15:37:55 +0100

    2-rectangle added condition if width or height is 0, perimeter is 0

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -71,4 +71,6 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width + self.__height) * 2
