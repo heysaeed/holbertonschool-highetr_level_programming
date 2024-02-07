1b4b2f728552bdd5e0b5ac2774a44e3d31f6dc6f
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-07 17:30:24 +0100

    3-say my name added comments

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """print my name"""
 
-def say_my_name(first_name, last_name=""):
 
-	if not isinstance(first_name, str):
-		raise TypeError("first_name must be a string")
-	if not isinstance(last_name, str):
-		raise TypeError("last_name must be a string")
-	print("My name is {} {}".format(first_name, last_name))
+def say_my_name(first_name, last_name=""):
+    """say My name is first_name last_name
+
+    Args:
+            first_name (str): explicit
+            last_name (str, optional): explicit. Defaults to "".
+
+    Raises:
+            TypeError: Need to be a str
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
