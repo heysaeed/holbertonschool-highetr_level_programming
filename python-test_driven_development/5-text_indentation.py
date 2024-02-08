8936efb61dcb3ce62d17855a008ab061be2a7cca
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-08 17:43:58 +0100

    update test with right answers

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
 def text_indentation(text):
-    characters = [".", ":", "!"]
-    new_string = ""
-
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for char in text:
-        new_string += char
-        for x in characters:
-            if x == char:
-                new_string += "\n"
-    return new_string
+
+    characters = [".", ":", "?"]
+    i = 0
+    while i < len(text):
+        if text[i] in characters:
+            print(text[i], end="")
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        print(text[i], end="")
+        i += 1
