ee17baa6b1cb3731eba5b72571aa72f3cc514e70
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-12 14:07:25 +0100

    realised what i did in 2-is_same_class is actually for the 3

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@ of the specified class,otherwise false	"""
 
 
 def is_same_class(obj, a_class):
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) == a_class
