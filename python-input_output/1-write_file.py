b699087eaaf727ca86c66dc432e4145fe28bfaf6
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-13 09:50:31 +0100

    added comment and return

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
 def write_file(filename="", text=""):
+    """write a file
+    (create one if none exist)
+
+        Args:
+                filename (str, optional): name of the file to
+                write into. Defaults to "".
+                text (str, optional): text to write into file.
+                Defaults to "".
+
+        Returns:
+                int: number of char written
+        """
     with open(filename, "w") as file:
-        file.write(text)
+        return file.write(text)
