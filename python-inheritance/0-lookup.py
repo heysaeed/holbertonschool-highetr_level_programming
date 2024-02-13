7adbc4c238ae13d691c8872b8a674b3e71efd17d
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-13 15:37:11 +0100

    fix comment 0-lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+return the list of available attributes and methods of an object
+"""
+
+
 def lookup(obj):
     """
     return the list of available attributes and methods of an object
