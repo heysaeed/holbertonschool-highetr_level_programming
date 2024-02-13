fdd601e0d2a8758430590c8386bed0a79fbf8208
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-13 15:48:03 +0100

    2-is_same_class fix return, was inverted

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,4 +8,4 @@ def is_same_class(obj, a_class):
     return True if object is exactly an instance
     of the specified class,otherwise false
     """
-    return type(obj) is not a_class
+    return type(obj) is a_class
