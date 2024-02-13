d836a0a6b6c8ace00dcb10e8ddc020acfb8b3476
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-13 17:52:14 +0100

    0-read_file added encoding in open file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    """read a file
+    """read a file and print its content
 
-        Args:
-                filename (str, optional): file to read and print. Defaults to "".
-        """
-    with open(filename) as file:
+    Args:
+            filename (str, optional): file to read and print.
+            Defaults to "".
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
         read_file = file.read()
-        print(read_file)
+        print(read_file, end='')
