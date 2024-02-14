1baa42b55ebc88f15c57110a471a64f68f76324f
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-14 14:44:14 +0100

    trying and remove __main__ condition

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -15,10 +15,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 file_name = "add_item.json"
 
 
-if __name__ == "__main__":
-    try:
-        json_file = load_from_json_file(file_name)
-    except FileNotFoundError:
-        json_file = []
+
+try:
+    json_file = load_from_json_file(file_name)
+except FileNotFoundError:
+    json_file = []
 json_file.extend(sys.argv[1:])
 save_to_json_file(json_file, file_name)
