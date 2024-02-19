862080325411e9f1e8d8fd667689f1e091a08fcc
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-19 17:19:57 +0100

    Added to_dictionary() to square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,3 +28,11 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 if key in attributes:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
