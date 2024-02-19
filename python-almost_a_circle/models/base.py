6f2d8e23c3fe3ce323041da64f1b056718347127
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-19 17:25:07 +0100

    added to_json_string() to base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 
 class Base:
 
@@ -10,3 +11,6 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        return json.dumps(list_dictionaries)
