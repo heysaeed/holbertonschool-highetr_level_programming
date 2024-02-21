c7dd90aac7ab620e5a5b92a65f4b7250fdbb1bb4
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-21 09:38:29 +0100

    added @staticmethod to to_json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Converts list of dictionaries to JSON string."""
         if not list_dictionaries:
